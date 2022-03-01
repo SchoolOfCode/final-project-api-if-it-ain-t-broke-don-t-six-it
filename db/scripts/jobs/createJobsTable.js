dd6e9d9ca602974b43dd2386bf9fe44150c4dd86
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-03-01 16:19:03 +0000

    changed data type of rate of pay in jobs table

--- db/scripts/jobs/createJobsTable.js
+++ db/scripts/jobs/createJobsTable.js
@@ -2,7 +2,7 @@ import query from "../../connection.js";
 
 async function createJobTable() {
   const response = await query(
-    `CREATE TABLE IF NOT EXISTS jobs (job_id SERIAL PRIMARY KEY, title VARCHAR (15) NOT NULL, date TEXT NOT NULL, rate_of_pay INT NOT NULL, description VARCHAR (2000) NOT NULL, requirement TEXT, user_id TEXT NOT NULL, accepted_user_id TEXT, status TEXT NOT NULL, user_image VARCHAR (2000),user_name TEXT NOT NULL, user_rating DECIMAL (2,1) NOT NULL, timestamp TIMESTAMPTZ NOT NULL DEFAULT(NOW()));`
+    `CREATE TABLE IF NOT EXISTS jobs (job_id SERIAL PRIMARY KEY, title VARCHAR (15) NOT NULL, date TEXT NOT NULL, rate_of_pay TEXT NOT NULL, description VARCHAR (2000) NOT NULL, requirement TEXT, user_id TEXT NOT NULL, accepted_user_id TEXT, status TEXT NOT NULL, user_image VARCHAR (2000),user_name TEXT NOT NULL, user_rating DECIMAL (2,1) NOT NULL, timestamp TIMESTAMPTZ NOT NULL DEFAULT(NOW()));`
   );
   console.log(response);
 }
